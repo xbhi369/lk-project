c555644aa4be07051ee8ad271fec97908147c1e7
xbhi369 <122586837+xbhi369@users.noreply.github.com>
2024-08-04 10:39:32 +0530

    Upqrcodegenerator.py

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -3,10 +3,8 @@
 # 2.Create a function that collects the  text and converts it to a qrcode
 # 3.Save the qrcode as an image
 # 4.run the function
-
 #1
 import qrcode #imports qrcode
-
 #2
 def generate_qrcode(text): #A function that takes in a text and converts it to a qrcode
 #3    
@@ -23,11 +21,28 @@ def generate_qrcode(text): #A function that takes in a text and converts it to a
 #6
     img = qr.make_image(fill_color="black", back_color="white")#creates an image of the QR code with black boxes and a white background.
 #7
-    img.save("qrcode5.png") #saves the generated QR code image
-        
+    img.save("qrcode007.png") #saves the generated QR code image        
 #8
 url = input("Enter your url:") #This line prompts the user to input URL
 generate_qrcode(url)  #Generates the QR Code
     
-    
-    
+#!!!!! ///THE QRCODE WILL BE SAVED ON THE FOLDER OF THIS FILE///     
+#If not working try changing the file name(img.save("qrcode006.png")) 
+#███╗   ███╗ █████╗ ██████╗ ███████╗    ██████╗ ██╗   ██╗
+#████╗ ████║██╔══██╗██╔══██╗██╔════╝    ██╔══██╗╚██╗ ██╔╝
+#██╔████╔██║███████║██║  ██║█████╗      ██████╔╝ ╚████╔╝ 
+#██║╚██╔╝██║██╔══██║██║  ██║██╔══╝      ██╔══██╗  ╚██╔╝  
+#██║ ╚═╝ ██║██║  ██║██████╔╝███████╗    ██████╔╝   ██║   
+#╚═╝     ╚═╝╚═╝  ╚═╝╚═════╝ ╚══════╝    ╚═════╝    ╚═╝   
+# █████╗ ██████╗ ██╗  ██╗██╗     ██╗██╗████████╗██╗  ██╗    ████████╗██████╗ 
+#██╔══██╗██╔══██╗██║  ██║██║     ██║██║╚══██╔══╝██║  ██║    ╚══██╔══╝██╔══██╗
+#███████║██████╔╝███████║██║     ██║██║   ██║   ███████║       ██║   ██████╔╝
+#██╔══██║██╔══██╗██╔══██║██║██   ██║██║   ██║   ██╔══██║       ██║   ██╔═══╝ 
+#██║  ██║██████╔╝██║  ██║██║╚█████╔╝██║   ██║   ██║  ██║       ██║   ██║     
+#╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝ ╚════╝ ╚═╝   ╚═╝   ╚═╝  ╚═╝       ╚═╝   ╚═╝     
+                                                                            
+                                                                                                       
+                                                                                                                                                                                                             
+                                                                                                       
+                                                                                                       
+   
